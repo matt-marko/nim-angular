f6d473116b1fdedaeb409dc1631bbd7ec4f1d806
Matt Marko <mattmarko321@gmail.com>
2024-07-20 07:14:13 -0700

    Update environment variables

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://nim-matt-backend-5601c5980271.herokuapp.com/high-scores'
+  apiUrl: 'https://nim-matt-backend-5601c5980271.herokuapp.com/high-scores',
+  webSocketUrl: 'wss://nim-matt-backend-5601c5980271.herokuapp.com/socket/'
 };
